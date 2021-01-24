ee9ba71941b509426b9a4fac72b5e5e5783f10a3
alexeybudyak <Alex42Olga>
2021-01-24 01:38:28 -0800

    add more options

--- src/Title/Language.js
+++ src/Title/Language.js
@@ -5,6 +5,13 @@ function Language(props) {
   return (
     <div>
         Language
+       <select>
+       <option value='Option 1' selected >Option 1</option>
+       <option value='Option 2' selected >Option 2</option>
+       <option value='Option 3' selected >Option 3</option>
+       <option value='Option 4' selected >Option 4</option>
+
+      </select>
     </div>
   );
 }
