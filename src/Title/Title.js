1de22fda1d1d9c89570071001a29dc79d4eab806
alexeybudyak <Alex42Olga>
2021-01-24 01:13:23 -0800

    added columns

--- src/Title/Title.js
+++ src/Title/Title.js
@@ -4,12 +4,26 @@ import Language from "./Language";
 
 function Title(props) {
   return (
-    <div>
-      <div className="alert alert-primary" role="alert">
-        Title
+       <div className="alert alert-primary" role="alert">
+
+    <div className="container">
+      <div className="row align-items-start">
+        <div className="col">
+          Title
+        </div>
+        <div className="col">
+
         <Language/>
+
+        </div>
+
+        <div className="col">
+          Count
+        </div>
       </div>
     </div>
+      </div>
+
   );
 }
 
