76bf3f339cf7f19f3ebce5c82c29958d7c0ab9c8
alexeybudyak <Alex42Olga>
2021-01-24 01:33:26 -0800

    update the Title

--- src/Title/Title.js
+++ src/Title/Title.js
@@ -4,25 +4,25 @@ import Language from "./Language";
 
 function Title(props) {
   return (
-       <div className="alert alert-primary" role="alert">
+    <div className="alert alert-primary" role="alert">
 
-    <div className="container">
-      <div className="row align-items-start">
-        <div className="col">
-          Title
-        </div>
-        <div className="col">
+      <div className="container">
+        <div className="row align-items-start">
+          <div className="col">
+            A Mazing Way
+          </div>
+          <div className="col">
 
-        <Language/>
+            <Language/>
 
-        </div>
+          </div>
 
-        <div className="col">
-          Count
+          <div className="col">
+            Count
+          </div>
         </div>
       </div>
     </div>
-      </div>
 
   );
 }
