eef284826ac555ae845d31b88361fb05c876fe5b
alexeybudyak <Alex42Olga>
2021-01-24 23:04:01 -0800

    Turn off Data from App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, {useState} from 'react';
 import Title from "./Title/Title";
-import QAData from "./Data/QAData.js";
-
 
 function App() {
-  const QA = QAData();
   const [lg,setLg] = useState(0);
   return (
     <div>
       <Title lg = {lg} setLg = {setLg} />
-      {QA[0][0][0].Q}
+
     </div>
   );
 }
