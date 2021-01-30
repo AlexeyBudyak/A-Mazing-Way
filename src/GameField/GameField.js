b801c149a9785cc4d7deb2012e28218dfe18249b
alexeybudyak <Alex42Olga>
2021-01-30 09:53:22 +0200

    done 1lvl data

--- src/GameField/GameField.js
+++ src/GameField/GameField.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import IconSquareFill from "../Icons/IconSquareFill";
 import "../CSS/gameField.css";
+import mazeData from "../Data/MazaData";
 
 
 function GameField(props) {
@@ -9,8 +10,14 @@ function GameField(props) {
   const blockBar = Array(28).fill(<tr>{blockLine}</tr>);
   for(let i = 1; i < 27; i++){
     let newLine = Array(42).fill(<td id='gPix'> </td>);
+
     newLine[1] = <td id='gClear'>.</td>;
-    newLine[40] = <td id='gClear'>.</td>;
+    for(let j = 0; j < 38; j++){
+      if( i <= mazeData(0).length && mazeData(0)[i - 1][j] === '0')  newLine[j + 2] = <td id='gClear'> </td>;
+      //newLine.push(<td id='gClear'>0</td>);
+    }
+    newLine[40] = <td id='gClear'> </td>;
+
     blockBar[i] = <tr>{newLine}</tr>;
   }
   return (
