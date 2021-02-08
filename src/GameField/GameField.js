1801223823a027e917f291995087e4eb80b03de3
alexeybudyak <Alex42Olga>
2021-02-08 08:42:46 +0200

    edit map style

--- src/GameField/GameField.js
+++ src/GameField/GameField.js
@@ -8,15 +8,24 @@ function GameField(props) {
   //
   const blockLine = Array(42).fill(<td id='gPix'> </td>);
   const blockBar = Array(28).fill(<tr>{blockLine}</tr>);
+  let tdId;
   for(let i = 1; i < 27; i++){
     let newLine = Array(42).fill(<td id='gPix'> </td>);
 
-    newLine[1] = <td id='gClear'>.</td>;
-    for(let j = 0; j < 38; j++){
-      if( i <= mazeData(0).length && mazeData(0)[i - 1][j] === '0')  newLine[j + 2] = <td id='gClear'> </td>;
+    //newLine[1] = <td id='gStart'>.</td>;
+    for(let j = 0; j < 40; j++){
+      if( i <= mazeData(0).length && mazeData(0)[i - 1][j] === '0') {
+        if(j===0) tdId = 'gStart';
+            else  if(j === 39)  tdId = 'gEnd';
+                        else    tdId = 'gClear';
+
+          newLine[j + 1] = <td id={tdId}></td>;
+
+
+      }
       //newLine.push(<td id='gClear'>0</td>);
     }
-    newLine[40] = <td id='gClear'> </td>;
+    //newLine[40] = <td id='gEnd'> </td>;
 
     blockBar[i] = <tr>{newLine}</tr>;
   }
