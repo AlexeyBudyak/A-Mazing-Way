ff29468714b6fc37a013c82167e37696a1a8e58c
alexeybudyak <Alex42Olga>
2021-03-05 17:45:50 -0800

    1st question generation

--- src/Title/Title.js
+++ src/Title/Title.js
@@ -5,13 +5,22 @@ import Skin from "./Skin";
 import lgData from "../Data/LanguageData";
 import Difficulty from "./Difficulty";
 
+function questionsGenerator(maze, setMaze, player){
+  const chance = [5,15,30,50,75][player.difficluty];
+  setMaze(maze.map(line=>line.split('')
+    .map((el,i)=>el === '0' && Math.random()*100 < chance && i && i < 39 ? 'A' : el).join(''))
+  );
+}
+
 function mazeStart(maze, setMaze, player, setPlayer){
+  questionsGenerator(maze, setMaze, player);
+
   let devX, devY;
   do {
     devX = 1 + ~~(Math.random()*38);
     devY = ~~(Math.random()*26);
   }while(maze[devY][devX]!=='0');
-  console.log(devX,':',devY);
+
   setPlayer({...player, x:0, y: 13, devX: devX, devY: devY, devActive: true});
   console.log(player);
 }
