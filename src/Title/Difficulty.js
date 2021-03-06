30710b5417354f2161e52aa2d1665134acd43f43
alexeybudyak <Alex42Olga>
2021-03-05 21:14:15 -0800

    difficulty bug fixed

--- src/Title/Difficulty.js
+++ src/Title/Difficulty.js
@@ -4,13 +4,14 @@ import lgData from "../Data/LanguageData";
 function Difficulty(props) {
   const {player, setPlayer}  = props;
   const diffLevels = lgData(player.lg).DiffLevels;
+  //console.log(player.difficluty)
     function diffHandler() {
       setPlayer({...player, difficluty: diffLevels.indexOf(document.getElementById("difficluty").value)});
     }
   return (
     <div>
       {lgData(player.lg).Difficluty}  &nbsp;
-       <select id='difficluty' onChange={()=>diffHandler()}>
+       <select id='difficluty' onChange={()=>diffHandler()} value={diffLevels[player.difficluty]}>
          {diffLevels.map((el,i) =>
             <option value={el} id={el} key={el} select={(i === player.difficluty)+''} >{el}</option>)}
       </select>
