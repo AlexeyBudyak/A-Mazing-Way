e6ea7e53ea16377750651bf69c8021e054df39af
alexeybudyak <Alex42Olga>
2021-04-05 00:59:57 -0700

    remove comments

--- src/GameField/GameField.js
+++ src/GameField/GameField.js
@@ -6,9 +6,7 @@ import setTrack from "./setTrack";
 function blockHandler(maze,setMaze, player, setPlayer, x2, y2){
   let x = player.x;
   let y = player.y;
-  console.log('target: ',maze[y2][x2]);
   player = {...player, target: {x:x2, y: y2, pic : maze[y2][x2],active:true}}
-  console.log(player.target);
   if(maze[y2][x2]!=='0')  maze[y2][x2] = '0';
   const path = setTrack(maze, player.x, player.y, x2, y2, 1);
   if(!path.length)  maze[y2][x2] = player.target.pic;
